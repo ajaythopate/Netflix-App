8b1498ac8ab8088c2521e4fb3e973f119536d336
ajaythopate <ajaythopate999@gmail.com>
2023-09-26 15:09:06 +0530

    background image and logo create

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Header from './Header';
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+      <Header />
+      <div>
+        <img src="https://miro.medium.com/v2/resize:fit:1400/1*5lyavS59mazOFnb55Z6znQ.png"
+          alt="logo" />
+      </div>
+    </div>
   )
 }
 
